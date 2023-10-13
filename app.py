7c5526077c6e7d5219b911fbe130b25080f18ebc
martimds <martimtimdias@gmail.com>
2023-10-12 22:25:51 -0300

    Implementado Flask

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/quemsomos")
+def quemsomos():
+    return render_template("quemsomos.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
